d48ae9f2b688cafc1a979b22c99bb951a0e480a8
Parsharouthu Praneeth <47476114+praneethvarma17@users.noreply.github.com>
2024-01-11 11:59:40 +0530

    Add paginator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { AdminData } from './app.interface';
+import {MatInputModule} from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatTableModule, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, MatTableModule, HttpClientModule, MatPaginatorModule, MatInputModule, MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,22 +20,29 @@ export class AppComponent {
   title = 'admin-ui';
   public displayedColumns: string[] = ['id', 'name', 'email', 'role'];
   private _actualData!: AdminData[];
-  public data!: AdminData[];
+  public data!: MatTableDataSource<AdminData>
 
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   ngOnInit() {
     this._http.get<AdminData[]>('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json').subscribe((res) => {
       this._actualData = res
-      this.data = res
+      this.data = new MatTableDataSource(res)
     })
+
   }
 
   triggerSearch($event: any) {
     console.log('Search Triggered')
     if ($event.target.value){
-      this.data = [...this._actualData.filter(row => row.id.includes($event.target.value)), ...this._actualData.filter(row => row.name.includes($event.target.value)), ...this._actualData.filter(row => row.email.includes($event.target.value)), ...this._actualData.filter(row => row.role.includes($event.target.value))]
+      this.data.data = [...this._actualData.filter(row => row.id.includes($event.target.value)), ...this._actualData.filter(row => row.name.includes($event.target.value)), ...this._actualData.filter(row => row.email.includes($event.target.value)), ...this._actualData.filter(row => row.role.includes($event.target.value))]
     }
      else {
-      this.data = this._actualData;
+      this.data.data = this._actualData;
      }
+  }
+
+  ngAfterViewInit() {
+    this.data.paginator = this.paginator
   }
 }
